eb543c4e0d3877b74a7344b1dbc6fbcd6fe4ffed
sholeh007 <sholeh09@outlook.com>
2021-03-26 16:41:16 +0900

    adding jwt

--- controller/Auth.js
+++ controller/Auth.js
@@ -1,9 +1,10 @@
 import User from "../models/userModel.js";
 import { validationResult } from "express-validator";
 import argon2 from "argon2";
+import jwt from "jsonwebtoken";
 
 const Auth = {
-  async signup(req, res, next) {
+  async signup(req, res) {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ error: errors.array() });
@@ -20,6 +21,29 @@ const Auth = {
       res.status(201).json({ message: "user created", userId: user._id });
     } catch (error) {
       res.status(500).json({ message: "internal error" });
+    }
+  },
+  async login(req, res) {
+    const { email, password } = req.body;
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(404).json({ message: "email not found" });
+      }
+
+      const decrypt = await argon2.verify(user.password, password);
+      if (!decrypt) {
+        return res.status(401).json({ message: "wrong password" });
+      }
+
+      const token = jwt.sign(
+        { email: user.email, id: user._id.toString() },
+        "somesupersecret",
+        { expiresIn: "1h" }
+      );
+      res.status(200).json({ token, userId: user._id.toString() });
+    } catch (error) {
+      res.status(500).json({ message: error });
     }
   },
 };
--- routes/auth.js
+++ routes/auth.js
@@ -5,5 +5,6 @@ import validation from "../helper/validation.js";
 const router = express.Router();
 
 router.put("/signup", validation.signup, controllerAuth.signup);
+router.post("/login", controllerAuth.login);
 
 export default router;
